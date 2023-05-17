f5cb6bc4f8cd776bccf57cb11b5fc1e1bcb95841
hamzaish <hamzaish@ad.unc.edu>
2023-05-17 18:10:26 -0400

    Test

---
+++ capture.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+import numpy as np
+
+cap = cv.VideoCapture(0)
+
+while(1):
+
+    # Take each frame
+    _, frame = cap.read()
+
+    # Convert BGR to HSV
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    # define range of blue color in HSV
+    lower_blue = np.array([110,50,50]) # Play with these numbers and see the outcome
+    upper_blue = np.array([130,255,255]) # Play with these values too! Play!
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv.inRange(hsv, lower_blue, upper_blue)
+
+    # Bitwise-AND mask and original image
+    res = cv.bitwise_and(frame,frame, mask= mask)
+
+    cv.imshow('frame',frame)
+    cv.imshow('mask',mask)
+    cv.imshow('res',res)
+    k = cv.waitKey(5) & 0xFF
+    if k == 27:
+    break
+
+    cv.destroyAllWindows()
